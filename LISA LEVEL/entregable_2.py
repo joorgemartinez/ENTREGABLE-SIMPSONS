a7ff1bec6276c2f67c76adf2dcb62c1480e70345
Jorge <martinezca.jorge@gmail.com>
2022-10-19 21:10:52 +0200

    commit

--- LISA LEVEL/entregable_2.py
+++ LISA LEVEL/entregable_2.py
@@ -43,22 +43,22 @@ while True:
       imagen_local = f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}/{personaje}.png"
 
       if personaje == "Lisa Simpson":
-        dict_2 = {"personaje": personaje, "frase": frase}
+        dict_1 = {"personaje": personaje, "frase": frase}
         with open(f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}/{personaje}.csv", 'a') as h: 
-          a = csv.DictWriter(h, dict_2.keys())
-          a.writerow(dict_2)
+          a = csv.DictWriter(h, dict_1.keys())
+          a.writerow(dict_1)
 
       elif personaje == "Homer Simpson":
-        dict_3 = {"personaje": personaje, "frase": frase}
+        dict_2 = {"personaje": personaje, "frase": frase}
         with open (f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}/{personaje}.csv", 'a') as g:
-          a = csv.DictWriter (g, dict_3.keys())
-          a.writerow(dict_3)
+          a = csv.DictWriter (g, dict_2.keys())
+          a.writerow(dict_2)
 
       else:
-        dict_1 = {"personaje": personaje, "frase": frase}
+        dict_3 = {"personaje": personaje, "frase": frase}
         with open(f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/general.csv", 'a') as f: 
-          a = csv.DictWriter(f, dict_1.keys())
-          a.writerow(dict_1)
+          a = csv.DictWriter(f, dict_3.keys())
+          a.writerow(dict_3)
       
       with open(imagen_local, 'wb') as handler:
         handler.write(imagen)
