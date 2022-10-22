4c490a7b8ee7a943c85df082c73838aa1a91372e
Jorge <martinezca.jorge@gmail.com>
2022-10-22 20:47:24 +0200

    update

--- LISA LEVEL/entregable_2.py
+++ LISA LEVEL/entregable_2.py
@@ -7,10 +7,7 @@ import csv
 import os
 import errno
 
-
-general_list = []
-homer_list = []
-lisa_list = []
+#Creamos el diccionario que albergará el conteo de las palabras. 
 cuenta_palabras = {}
 
 while True:
@@ -20,47 +17,38 @@ while True:
     
   frase: str = datos[0]["quote"]
   personaje: str = datos[0]["character"]
-    
+
   #Obtenemos la url de la imagen y la guardamos en el directorio
 
   imagen_url: str = datos [0]["image"]
   imagen_local = f'{personaje}.png'
   imagen = requests.get (imagen_url).content
 
-  if personaje == "Lisa Simpson":
-    general_list.append((personaje, frase))
-    lisa_list.append((personaje,frase))
-        
-  elif personaje == "Homer Simpson":
-    general_list.append((personaje,frase))
-    homer_list.append((personaje,frase))
-        
-  else:
-    general_list.append((personaje,frase))
     
-
+  #Creamos carpetas para cada cliente en el que se introducirá su imagen.
   try:
       os.mkdir(f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}")
       imagen_local = f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}/{personaje}.png"
 
+      dict_General = {"personaje": personaje, "frase": frase}
+      with open(f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/general.csv", 'a') as f: 
+        a = csv.DictWriter(f, dict_General.keys())
+        a.writerow(dict_General)
+
+
+      #Cuando el personaje es Lisa o Homer, introducimos también el archivo .csv dentro de la carpeta.
       if personaje == "Lisa Simpson":
-        dict_1 = {"personaje": personaje, "frase": frase}
+        dict_Lisa = {"personaje": personaje, "frase": frase}
         with open(f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}/{personaje}.csv", 'a') as h: 
-          a = csv.DictWriter(h, dict_1.keys())
-          a.writerow(dict_1)
+          a = csv.DictWriter(h, dict_Lisa.keys())
+          a.writerow(dict_Lisa)
 
       elif personaje == "Homer Simpson":
-        dict_2 = {"personaje": personaje, "frase": frase}
+        dict_Homer = {"personaje": personaje, "frase": frase}
         with open (f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/{personaje}/{personaje}.csv", 'a') as g:
-          a = csv.DictWriter (g, dict_2.keys())
-          a.writerow(dict_2)
+          a = csv.DictWriter (g, dict_Homer.keys())
+          a.writerow(dict_Homer)
 
-      else:
-        dict_3 = {"personaje": personaje, "frase": frase}
-        with open(f"/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/general.csv", 'a') as f: 
-          a = csv.DictWriter(f, dict_3.keys())
-          a.writerow(dict_3)
-      
       with open(imagen_local, 'wb') as handler:
         handler.write(imagen)
         
@@ -68,4 +56,23 @@ while True:
     if e.errno != errno.EEXIST:
       raise
         
-    time.sleep (0)
+
+  #Con la función frase.split() separamos cada palabra y se guarda en lista
+  palabras = frase.split()
+  
+  #Mediante el bucle vamos actualizando el valor del diccionario
+  for palabra in palabras:
+    value = 0
+
+    if palabra not in cuenta_palabras:
+      cuenta_palabras[palabra] = value
+
+    value = palabras.count(palabra)
+    cuenta_palabras[palabra] += value
+
+  #Creación del archivo .txt en el que se introducirá el conteo de las palabras
+  with open ('/Users/jorgemartinezcanet/Documents/GitHub/ENTREGABLE-SIMPSONS/LISA LEVEL/ContadorPalabras.txt', 'w') as cuentapalabras:
+      for clave, valor in cuenta_palabras.items():
+        cuentapalabras.write(f"\n{clave}: {valor}")
+
+  time.sleep (0)
